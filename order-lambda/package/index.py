9787ba24b5a9abaff9868704685ea2c3352cf7f3
Marru Manogna (Omnicom) <marru.manogna@omc.com>
2026-09-14 22:49:14 +0530

    Use environment for order Lambda SSM paths

--- order-lambda/package/index.py
+++ order-lambda/package/index.py
@@ -29,28 +29,30 @@ def response(status_code, message, data=None):
 # =====================================================
 def get_db_connection():
 
+    environment = os.environ["ENVIRONMENT"]
+    
     host = ssm_client.get_parameter(
-        Name="/cloudmart/dev/rds/host",
+        Name=f"/cloudmart/{environment}/rds/host",
         WithDecryption=True
     )["Parameter"]["Value"]
 
     port = ssm_client.get_parameter(
-        Name="/cloudmart/dev/rds/port",
+        Name=f"/cloudmart/{environment}/rds/port"
         WithDecryption=True
     )["Parameter"]["Value"]
 
     username = ssm_client.get_parameter(
-        Name="/cloudmart/dev/rds/username",
+        Name=f"/cloudmart/{environment}/rds/username"
         WithDecryption=True
     )["Parameter"]["Value"]
 
     password = ssm_client.get_parameter(
-        Name="/cloudmart/dev/rds/password",
+        Name=f"/cloudmart/{environment}/rds/password"
         WithDecryption=True
     )["Parameter"]["Value"]
 
     database = ssm_client.get_parameter(
-        Name="/cloudmart/dev/rds/db-name",
+        Name=f"/cloudmart/{environment}/rds/db-name"
         WithDecryption=True
     )["Parameter"]["Value"]
 
